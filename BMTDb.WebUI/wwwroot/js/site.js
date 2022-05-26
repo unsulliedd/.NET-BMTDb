e8e0ae6169248a8187cda771b2b59b2a69ccd30c
Berk Karakaş <berkkarakas98@hotmail.com>
2022-05-26 22:02:00 +0300

    Added hide navbar when scroll script

--- BMTDb.WebUI/wwwroot/js/site.js
+++ BMTDb.WebUI/wwwroot/js/site.js
@@ -1,1 +1,19 @@
-﻿
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+/************** Hide Navbar When Scroll **************/
+
+var firstScrollPosition = window.pageYOffset;           //Assigns page height to 'firstScrollPosition'
+
+window.onscroll = function ()                           //Function
+{
+    var currentScrollPosition = window.pageYOffset;
+    if (firstScrollPosition > currentScrollPosition) {
+        document.getElementById("navbar").style.top = "0";          //Shows navbar
+    } else {
+        document.getElementById("navbar").style.top = "-60px";      //Hides Navbar
+    }
+    firstScrollPosition = currentScrollPosition;        //Update scroll position
+}
+
+/************** Hide Navbar When Scroll **************/
