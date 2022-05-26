9d3fa903f16edb283ded5aef9b911881f41c0bc8
Berk Karakaş <berkkarakas98@hotmail.com>
2022-05-26 21:36:21 +0300

    Add project files.

---
+++ BMTDb.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BMTDb.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
