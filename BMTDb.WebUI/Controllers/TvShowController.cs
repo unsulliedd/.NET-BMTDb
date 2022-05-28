0ba979e51f9db059442e1c7cc51f074da29ab7a3
Berk Karakaş <berkkarakas98@hotmail.com>
2022-05-28 21:33:30 +0300

    Added new controllers

---
+++ BMTDb.WebUI/Controllers/TvShowController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BMTDb.WebUI.Controllers
+{
+    public class TvShowController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
