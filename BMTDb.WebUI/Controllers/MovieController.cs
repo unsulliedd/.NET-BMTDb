1527bdc758bc55a5343d381f9a6699ccc89ef855
Berk Karakaş <berkkarakas98@hotmail.com>
2022-05-30 23:54:56 +0300

    Dependency Injection

--- BMTDb.WebUI/Controllers/MovieController.cs
+++ BMTDb.WebUI/Controllers/MovieController.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor.
+
+using BMTDb.Data.Abstract;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BMTDb.WebUI.Controllers
 {
     public class MovieController : Controller
     {
+        private readonly IMovieRepository _movieRepository;
+        public MovieController(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+
         public IActionResult Index()
         {
             return View();
