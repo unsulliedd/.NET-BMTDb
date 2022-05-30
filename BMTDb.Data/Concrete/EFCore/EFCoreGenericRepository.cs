563f449e1dfca12aca7d3fcc76fc828f03b36b4a
Berk Karakaş <berkkarakas98@hotmail.com>
2022-05-30 22:57:34 +0300

    Generic concrete class

---
+++ BMTDb.Data/Concrete/EFCore/EFCoreGenericRepository.cs
@@ -0,0 +1,60 @@
+﻿#pragma warning disable IDE0063
+#pragma warning disable CS8603
+
+using BMTDb.Data.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMTDb.Data.Concrete.EFCore
+{
+    public class EFCoreGenericRepository<TEntity, TContext> : IRepository<TEntity>
+        where TEntity  : class
+        where TContext : DbContext, new()
+    {
+        public List<TEntity> GetAll()
+        {
+            using (var context = new TContext())
+            {
+                return context.Set<TEntity>().ToList();
+            }
+        }
+
+        public TEntity GetById(int id)
+        {
+            using (var context = new TContext())
+            {
+                return context.Set<TEntity>().Find(id);
+            }
+        }
+        public void Create(TEntity entity)
+        {
+            using (var context = new TContext())
+            {
+                context.Set<TEntity>().Add(entity);
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (var context = new TContext())
+            {
+                context.Set<TEntity>().Remove(entity);
+                context.SaveChanges();
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (var context = new TContext())
+            {
+                context.Entry(entity).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
