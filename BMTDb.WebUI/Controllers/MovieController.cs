e2f936b9c86628b6c9187475814ab8bb09a02ae3
Berk Karakaş <berkkarakas98@hotmail.com>
2022-06-01 00:33:19 +0300

    Update - Using service class

--- BMTDb.WebUI/Controllers/MovieController.cs
+++ BMTDb.WebUI/Controllers/MovieController.cs
@@ -1,16 +1,14 @@
-﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor.
-
-using BMTDb.Data.Abstract;
+﻿using BMTDb.Service.Abstract;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BMTDb.WebUI.Controllers
 {
     public class MovieController : Controller
     {
-        private readonly IMovieRepository _movieRepository;
-        public MovieController(IMovieRepository movieRepository)
+        private readonly IMovieService _movieService;
+        public MovieController(IMovieService movieService)
         {
-            _movieRepository = movieRepository;
+            _movieService = movieService;
         }
 
         public IActionResult Index()
