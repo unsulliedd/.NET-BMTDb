214ecdc38e044341140b77a120b54f76b553aaaf
Berk Karakaş <berkkarakas98@hotmail.com>
2022-06-02 00:19:46 +0300

    Added MovieViewModel

---
+++ BMTDb.WebUI/Models/MovieViewModel.cs
@@ -0,0 +1,10 @@
+﻿#pragma warning disable CS8618
+using BMTDb.Entity;
+
+namespace BMTDb.WebUI.Models
+{
+    public class MovieViewModel
+    {
+        public List<Movie> Movies { get; set; }
+    }
+}
