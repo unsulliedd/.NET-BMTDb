f6a858df4fdd5ebd12694d4b900022b2ffaadb44
Berk Karakaş <berkkarakas98@hotmail.com>
2022-06-03 01:26:50 +0300

    Added - Movie Details

--- BMTDb.WebUI/Controllers/MovieController.cs
+++ BMTDb.WebUI/Controllers/MovieController.cs
@@ -1,4 +1,5 @@
-﻿using BMTDb.Service.Abstract;
+﻿using BMTDb.Entity;
+using BMTDb.Service.Abstract;
 using BMTDb.WebUI.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,6 +20,28 @@ namespace BMTDb.WebUI.Controllers
                 Movies = _movieService.GetAll()
             };
             return View(movieViewModel);
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return View("Error");
+            }
+
+            Movie movie = _movieService.GetMovieDetails((int)id);
+
+            if (id == null)
+            {
+                return View("Error");
+            }
+            return View(new MovieDetailModel
+            {
+                Movie = movie,
+                Genres = movie.MovieGenres.Select(i => i.Genre).ToList(),
+                Studios = movie.MovieStudios.Select(i => i.Studios).ToList(),
+                Persons = movie.MovieCrews.Select(i => i.Person).ToList()
+            });
         }
     }
 }
