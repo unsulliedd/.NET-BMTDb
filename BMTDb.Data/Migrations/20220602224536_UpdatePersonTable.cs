d7a6285c9629758580190af8f21a3d1bf1cca3d0
Berk Karakaş <berkkarakas98@hotmail.com>
2022-06-03 01:47:18 +0300

    Update - Migration

---
+++ BMTDb.Data/Migrations/20220602224536_UpdatePersonTable.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BMTDb.Data.Migrations
+{
+    public partial class UpdatePersonTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Deathday",
+                table: "Persons",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoUrl",
+                table: "Persons",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Deathday",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "PhotoUrl",
+                table: "Persons");
+        }
+    }
+}
