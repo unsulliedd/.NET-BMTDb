0765864f13decd95d3256c2cc48bb045949b7b4d
Berk Karakaş <berkkarakas98@hotmail.com>
2022-06-02 23:47:30 +0300

    Added Accordion Script

--- BMTDb.WebUI/wwwroot/js/site.js
+++ BMTDb.WebUI/wwwroot/js/site.js
@@ -56,3 +56,23 @@ darkModeToggle.addEventListener('click', () => {        // When someone clicks t
 );
 
 /************** Dark Mode Script **************/
+
+
+/* ************ Accordion Script ************ */
+
+document.querySelectorAll('.accordion_button').forEach(button => {
+    button.addEventListener('click', () => {
+        const accordionContent = button.nextElementSibling;
+
+        button.classList.toggle('accordion_button--active');
+
+        if (button.classList.contains('accordion_button--active')) {
+            accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+        }
+        else {
+            accordionContent.style.maxHeight = 0;
+        }
+    });
+});
+
+/* ************ Accordion Script ************ */
