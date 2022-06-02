2f7d4e9276d4f89d798285ec759a9e8256a7e00a
Berk Karakaş <berkkarakas98@hotmail.com>
2022-06-03 01:27:18 +0300

    Added - MovieDetailModel

---
+++ BMTDb.WebUI/Models/MovieDetailModel.cs
@@ -0,0 +1,14 @@
+﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor.
+
+using BMTDb.Entity;
+
+namespace BMTDb.WebUI.Models
+{
+    public class MovieDetailModel
+    {
+        public Movie Movie { get; set; }
+        public List<Genre> Genres { get; set; }
+        public List<Studio> Studios { get; set; }
+        public List<Person> Persons { get; set; }
+    }
+}
