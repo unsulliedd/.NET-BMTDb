56e47b53d7294aec125f0a866453570a24c6c552
Berk Karakaş <berkkarakas98@hotmail.com>
2022-06-09 00:53:03 +0300

    Update - Migration

---
+++ BMTDb.Data/Migrations/20220606224515_UpdateDateTime.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BMTDb.Data.Migrations
+{
+    public partial class UpdateDateTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
