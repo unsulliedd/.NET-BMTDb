b3a02fa3a717925d5a2a375e47d446cbbea1222e
Berk Karakaş <berkkarakas98@hotmail.com>
2022-06-14 20:29:33 +0300

    Added JavaScript to Popup Notifications

--- BMTDb.WebUI/wwwroot/js/site.js
+++ BMTDb.WebUI/wwwroot/js/site.js
@@ -1,6 +1,15 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
+/******************** Close Popup ********************/
+
+$(".msgbox-close").click(function () {
+    $(".msgbox").fadeOut("fast");
+});
+
+/******************** Close Popup ********************/
+
+
 /************** Hide Navbar When Scroll **************/
 
 var firstScrollPosition = window.pageYOffset;                       //Assigns page height to 'firstScrollPosition'
