4c1ac77f244083086c2d086d5591f53175222e9e
Berk Karakaş <berkkarakas98@hotmail.com>
2022-06-15 22:13:13 +0300

    Added - User Class

---
+++ BMTDb.WebUI/Identity/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace BMTDb.WebUI.Identity
+{
+    public class User : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Gender { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime?  Birthday { get; set; }
+    }
+}
