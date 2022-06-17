8e96f5242296ee2a8a798b792936141b440a3247
Berk Karakaş <berkkarakas98@hotmail.com>
2022-06-17 22:57:55 +0300

    Added - ProfilePic Migration

---
+++ BMTDb.WebUI/Migrations/20220617155911_Added user profile.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BMTDb.WebUI.Migrations
+{
+    public partial class Addeduserprofile : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProfilePic",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilePic",
+                table: "AspNetUsers");
+        }
+    }
+}
