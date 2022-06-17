8b1750f3fe9b2986ab67f78f56b81618cde70c38
Berk Karakaş <berkkarakas98@hotmail.com>
2022-06-17 17:57:29 +0300

    IdentityOption Change

--- BMTDb.WebUI/Program.cs
+++ BMTDb.WebUI/Program.cs
@@ -24,7 +24,9 @@ builder.Services.Configure<IdentityOptions>(options => {
     options.Password.RequireDigit = true;
     options.Password.RequireLowercase = true;
     options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = false;
     options.Password.RequiredLength = 8;
+
 
     // SignIn
     options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
