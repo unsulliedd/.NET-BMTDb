cfb26b2a8d24a27c99b078a2a1058d9e2a84a485
Berk Karakaş <berkkarakas98@hotmail.com>
2022-07-04 00:02:25 +0300

    ViewComponents for Profile Header and Profile Image on Navbar

---
+++ BMTDb.WebUI/ViewComponents/ProfileHeaderViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using BMTDb.WebUI.Identity;
+using BMTDb.WebUI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BMTDb.WebUI.ViewComponents
+{
+    public class ProfileHeaderViewComponent : ViewComponent
+    {
+        private readonly UserManager<User> _userManager;
+        public ProfileHeaderViewComponent(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var userid = _userManager.GetUserId(HttpContext.User);
+            var user = await _userManager.FindByIdAsync(userid);
+
+            return View(new UserProfileModel()
+            {
+                UserName = user.UserName,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Gender = user.Gender,
+                ProfilePic = user.ProfilePic,
+                CreationDate = user.CreationDate,
+            });
+        }
+    }
+}
---
+++ BMTDb.WebUI/ViewComponents/ProfileNavbarViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using BMTDb.WebUI.Identity;
+using BMTDb.WebUI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BMTDb.WebUI.ViewComponents
+{
+    public class ProfileNavbarViewComponent : ViewComponent
+    {
+        private readonly UserManager<User> _userManager;
+        public ProfileNavbarViewComponent(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var userid = _userManager.GetUserId(HttpContext.User);
+            var user = await _userManager.FindByIdAsync(userid);
+
+            return View(new UserProfileModel()
+            {
+                UserName = user.UserName,
+                ProfilePic = user.ProfilePic,
+            });
+        }
+    }
+}
