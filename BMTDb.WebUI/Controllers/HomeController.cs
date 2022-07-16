c6ca2bad0ab0b0240973249793f0ede4d544e3f2
Berk Karakaş <berkkarakas98@hotmail.com>
2022-07-17 00:23:35 +0300

    Added - Api Call for TMDb Lists

--- BMTDb.WebUI/Controllers/HomeController.cs
+++ BMTDb.WebUI/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿#pragma warning disable IDE0052 // Remove unread private members
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
 
 using BMTDb.Service.Abstract;
 using BMTDb.WebUI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace BMTDb.WebUI.Controllers
@@ -18,13 +20,39 @@ namespace BMTDb.WebUI.Controllers
             _movieService = movieService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> IndexAsync()
         {
-            var movieViewModel = new MovieViewModel()
+            string apiKey = "e9e71fba25c4cdd53c7ced78867ba28d";
+
+            var baseAddress = new Uri("http://api.themoviedb.org/3/");
+            using var httpClient = new HttpClient { BaseAddress = baseAddress };
+
+            httpClient.DefaultRequestHeaders.TryAddWithoutValidation("accept", "application/json");
+
+            using var responsePopular = await httpClient.GetAsync("movie/popular?api_key=" + apiKey + "&language=en-US&page=1");
+            using var responseUpcoming = await httpClient.GetAsync("movie/upcoming?api_key=" + apiKey + "&language=en-US&page=1");
+            using var responseAiringTodayTv = await httpClient.GetAsync("tv/airing_today?api_key=" + apiKey + "&language=en-US&page=1");
+            using var responseOnAirTv = await httpClient.GetAsync("tv/on_the_air?api_key=" + apiKey + "&language=en-US&page=2");
+
+            string apiResponsePopular = await responsePopular.Content.ReadAsStringAsync();
+            string apiResponseUpcoming = await responseUpcoming.Content.ReadAsStringAsync();
+            string apiResponseAiringTodayTv = await responseAiringTodayTv.Content.ReadAsStringAsync();
+            string apiResponseOnAirTv = await responseOnAirTv.Content.ReadAsStringAsync();
+
+            TMDBApiPopular? rootObjectPopular = JsonConvert.DeserializeObject<TMDBApiPopular>(apiResponsePopular);
+            TMDBApiUpcoming? rootObjectUpcoming = JsonConvert.DeserializeObject<TMDBApiUpcoming>(apiResponseUpcoming);
+            TMDBApiAiringTodayTv? rootObjectAiringTodayTv = JsonConvert.DeserializeObject<TMDBApiAiringTodayTv>(apiResponseAiringTodayTv);
+            TMDBApiOnAirTv? rootObjectOnAirTv = JsonConvert.DeserializeObject<TMDBApiOnAirTv>(apiResponseOnAirTv);
+
+            TMDBListsApiResults lists = new()
             {
+                TMDBApiPopular = rootObjectPopular.results,
+                TMDBApiUpcoming = rootObjectUpcoming.results,
+                TMDBApiAiringTodayTv = rootObjectAiringTodayTv.results,
+                TMDBApiOnAirTv = rootObjectOnAirTv.results,
                 Movies = _movieService.GetByPopularity()
             };
-            return View(movieViewModel);
+            return View(lists);
         }
 
         public IActionResult About()
---
+++ BMTDb.WebUI/Models/TMDbListApiModel.cs
@@ -0,0 +1,89 @@
+﻿#pragma warning disable IDE1006 // Naming Styles
+
+using BMTDb.Entity;
+
+namespace BMTDb.WebUI.Models
+{
+    public class ListsMovieResult
+    {
+        public bool adult { get; set; }
+        public string? backdrop_path { get; set; }
+        public List<int>? genre_ids { get; set; }
+        public int id { get; set; }
+        public string? original_language { get; set; }
+        public string? original_title { get; set; }
+        public string? overview { get; set; }
+        public double? popularity { get; set; }
+        public string? poster_path { get; set; }
+        public string? release_date { get; set; }
+        public string? title { get; set; }
+        public bool video { get; set; }
+        public double? vote_average { get; set; }
+        public int? vote_count { get; set; }
+    }
+
+    public class ListTvResult
+    {
+        public string? backdrop_path { get; set; }
+        public string? first_air_date { get; set; }
+        public List<int>? genre_ids { get; set; }
+        public int id { get; set; }
+        public string? name { get; set; }
+        public List<string>? origin_country { get; set; }
+        public string? original_language { get; set; }
+        public string? original_name { get; set; }
+        public string? overview { get; set; }
+        public double? popularity { get; set; }
+        public string? poster_path { get; set; }
+        public double? vote_average { get; set; }
+        public int? vote_count { get; set; }
+    }
+    public class TMDBApiOnAirTv
+    {
+        public int page { get; set; }
+        public List<ListTvResult>? results { get; set; }
+        public int total_pages { get; set; }
+        public int total_results { get; set; }
+    }
+    public class TMDBApiAiringTodayTv
+    {
+        public int page { get; set; }
+        public List<ListTvResult>? results { get; set; }
+        public int total_pages { get; set; }
+        public int total_results { get; set; }
+    }
+
+    public class TMDBApiPopular
+    {
+        public int page { get; set; }
+        public List<ListsMovieResult>? results { get; set; }
+        public int total_pages { get; set; }
+        public int total_results { get; set; }
+    }
+
+    public class TMDBApiUpcoming
+    {
+        public int page { get; set; }
+        public List<ListsMovieResult>? results { get; set; }
+        public int total_pages { get; set; }
+        public int total_results { get; set; }
+    }
+
+    public class TMDBApiRecommendation
+    {
+        public int page { get; set; }
+        public List<ListsMovieResult>? results { get; set; }
+        public int total_pages { get; set; }
+        public int total_results { get; set; }
+    }
+
+    public class TMDBListsApiResults
+    {
+        public List<ListsMovieResult>? TMDBApiUpcoming { get; set; }
+        public List<ListsMovieResult>? TMDBApiPopular { get; set; }
+        public List<ListTvResult>? TMDBApiOnAirTv { get; set; }
+        public List<ListTvResult>? TMDBApiAiringTodayTv { get; set; }
+
+        public List<Movie>? Movies { get; set; }
+    }
+}
