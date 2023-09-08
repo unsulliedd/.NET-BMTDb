6075b2219bd2ae62627c756df428b892c7e6cc9d
Berk <berkkarakas98@hotmail.com>
2023-09-08 21:40:51 +0300

    Add null checks for API calls
    - Remove old TMDB API key

--- BMTDb.WebUI/Controllers/HomeController.cs
+++ BMTDb.WebUI/Controllers/HomeController.cs
@@ -22,37 +22,40 @@ namespace BMTDb.WebUI.Controllers
 
         public async Task<IActionResult> IndexAsync()
         {
-            string apiKey = "e9e71fba25c4cdd53c7ced78867ba28d";
+            string apiKey = "";
 
             var baseAddress = new Uri("http://api.themoviedb.org/3/");
             using var httpClient = new HttpClient { BaseAddress = baseAddress };
 
             httpClient.DefaultRequestHeaders.TryAddWithoutValidation("accept", "application/json");
-
-            using var responsePopular = await httpClient.GetAsync("movie/popular?api_key=" + apiKey + "&language=en-US&page=1");
-            using var responseUpcoming = await httpClient.GetAsync("movie/upcoming?api_key=" + apiKey + "&language=en-US&page=1");
-            using var responseAiringTodayTv = await httpClient.GetAsync("tv/airing_today?api_key=" + apiKey + "&language=en-US&page=1");
-            using var responseOnAirTv = await httpClient.GetAsync("tv/on_the_air?api_key=" + apiKey + "&language=en-US&page=2");
-
-            string apiResponsePopular = await responsePopular.Content.ReadAsStringAsync();
-            string apiResponseUpcoming = await responseUpcoming.Content.ReadAsStringAsync();
-            string apiResponseAiringTodayTv = await responseAiringTodayTv.Content.ReadAsStringAsync();
-            string apiResponseOnAirTv = await responseOnAirTv.Content.ReadAsStringAsync();
-
-            TMDBApiPopular? rootObjectPopular = JsonConvert.DeserializeObject<TMDBApiPopular>(apiResponsePopular);
-            TMDBApiUpcoming? rootObjectUpcoming = JsonConvert.DeserializeObject<TMDBApiUpcoming>(apiResponseUpcoming);
-            TMDBApiAiringTodayTv? rootObjectAiringTodayTv = JsonConvert.DeserializeObject<TMDBApiAiringTodayTv>(apiResponseAiringTodayTv);
-            TMDBApiOnAirTv? rootObjectOnAirTv = JsonConvert.DeserializeObject<TMDBApiOnAirTv>(apiResponseOnAirTv);
-
-            TMDBListsApiResults lists = new()
+            if (apiKey != null)
             {
-                TMDBApiPopular = rootObjectPopular.results,
-                TMDBApiUpcoming = rootObjectUpcoming.results,
-                TMDBApiAiringTodayTv = rootObjectAiringTodayTv.results,
-                TMDBApiOnAirTv = rootObjectOnAirTv.results,
-                Movies = _movieService.GetByPopularity()
-            };
-            return View(lists);
+                using var responsePopular = await httpClient.GetAsync("movie/popular?api_key=" + apiKey + "&language=en-US&page=1");
+                using var responseUpcoming = await httpClient.GetAsync("movie/upcoming?api_key=" + apiKey + "&language=en-US&page=1");
+                using var responseAiringTodayTv = await httpClient.GetAsync("tv/airing_today?api_key=" + apiKey + "&language=en-US&page=1");
+                using var responseOnAirTv = await httpClient.GetAsync("tv/on_the_air?api_key=" + apiKey + "&language=en-US&page=2");
+
+                string apiResponsePopular = await responsePopular.Content.ReadAsStringAsync();
+                string apiResponseUpcoming = await responseUpcoming.Content.ReadAsStringAsync();
+                string apiResponseAiringTodayTv = await responseAiringTodayTv.Content.ReadAsStringAsync();
+                string apiResponseOnAirTv = await responseOnAirTv.Content.ReadAsStringAsync();
+
+                TMDBApiPopular? rootObjectPopular = JsonConvert.DeserializeObject<TMDBApiPopular>(apiResponsePopular);
+                TMDBApiUpcoming? rootObjectUpcoming = JsonConvert.DeserializeObject<TMDBApiUpcoming>(apiResponseUpcoming);
+                TMDBApiAiringTodayTv? rootObjectAiringTodayTv = JsonConvert.DeserializeObject<TMDBApiAiringTodayTv>(apiResponseAiringTodayTv);
+                TMDBApiOnAirTv? rootObjectOnAirTv = JsonConvert.DeserializeObject<TMDBApiOnAirTv>(apiResponseOnAirTv);
+
+                TMDBListsApiResults lists = new()
+                {
+                    TMDBApiPopular = rootObjectPopular.results,
+                    TMDBApiUpcoming = rootObjectUpcoming.results,
+                    TMDBApiAiringTodayTv = rootObjectAiringTodayTv.results,
+                    TMDBApiOnAirTv = rootObjectOnAirTv.results,
+                    Movies = _movieService.GetByPopularity()
+                };
+                return View(lists);
+            }
+            return View();
         }
 
         public IActionResult About()
